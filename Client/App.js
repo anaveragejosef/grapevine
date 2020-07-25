b370b95da6f281253b545fbb1a735506858ea477
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-07-25 14:38:28 -0700

    Moves App File
    - Moves App file into a Client folder
    - Updates the index.js file so it will continue to point at file
    - Updates the App file so that it uses the Navigation Container so that
    I will be able to move between pages on the application

---
+++ Client/App.js
@@ -0,0 +1,53 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './Components/HomeScreen.js';
+import WineType from './Components/WineType.js';
+import WineVarietal from './Components/WineVarietal.js';
+import WineList from './Components/WineList.js';
+import WineBottle from './Components/WineBottle.js';
+import WineForm from './Components/WineForm.js';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Welcome to Grapevine!' }}
+        />
+        <Stack.Screen
+          name="WineForm"
+          component={WineForm}
+          options={{ title: 'Add a New Wine' }}
+        />
+        <Stack.Screen
+          name="WineType"
+          component={WineType}
+          options={{ title: 'Select a Wine Type' }}
+        />
+        <Stack.Screen
+          name="WineVarietal"
+          component={WineVarietal}
+          options={{ title: 'Select a Varietal' }}
+        />
+        <Stack.Screen
+          name="WineList"
+          component={WineList}
+          options={{ title: 'Your Wine List' }}
+        />
+        <Stack.Screen
+          name="WineBottle"
+          component={WineBottle}
+          options={{ title: 'Wine Review' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
