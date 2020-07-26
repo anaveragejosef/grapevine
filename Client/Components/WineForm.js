a418c206b8df22f5a9f4a9cbac80bf96a59435d8
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-07-26 12:03:19 -0700

    Adds the Initial Form Framework/Logic
    - Constructs state for the form using React Hooks
    - Adds form layout that is built on the TextInput fields and has basic
    validation
    - Adds a submit button that will validate that the fields are filled,
    calculates the wine type based on varietal, and sends a POST request to
    the Express server
    - Adds Picker package to the application (package.json/podfile.lock)

--- Client/Components/WineForm.js
+++ Client/Components/WineForm.js
@@ -1,15 +1,138 @@
 import 'react-native-gesture-handler';
-import React from 'react';
-import { SafeAreaView, StyleSheet, View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useState } from 'react';
+import { SafeAreaView, ScrollView, View, TextInput, Text, Button } from 'react-native';
+import { Picker } from '@react-native-community/picker';
+import axios from 'axios';
 
-const WineForm = () => {
+const WineForm = ({ navigation }) => {
+  const [winery, setWinery] = useState('');
+  const [name, setName] = useState('');
+  const [varietal, setVarietal] = useState('');
+  const [vintage, setVintage] = useState('');
+  const [notes, setNotes] = useState('');
+  const [purchaseAgain, setRepeat] = useState('Yes');
+
+  const submitWine = () => {
+    if (validateInputs()) {
+      let type = getWineType();
+      fullSend(type);
+    } else {
+      return;
+    }
+  }
+
+  const validateInputs = () => {
+    if (winery === '' || name === '' || varietal === '' || vintage === '') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  const getWineType = () => {
+    const reds = ['Cabernet Franc', 'Cabernet Sauvignon', 'Grenache', 'Malbec', 'Merlot', 'Mourvèdre', 'Petite Sirah', 'Petite Verdot', 'Pinot Noir', 'Sangiovese', 'Syrah', 'Red Blend', 'Tempranillo', 'Zinfandel'];
+    const whites = ['Chardonnay', 'Sauvignon Blanc', 'Pinot Grigio', 'Pinot Gris', 'Riesling', 'Muscat/Moscato', 'White Blend', 'Pinot Blanc'];
+    const roses = ['Grenache Rosé', 'Sangiovese Rosé', 'Tempranillo Rosé', 'Syrah Rosé', 'Cabernet Sauvignon Rosé', 'Zinfandel Rosé', 'Tavel Rosé', 'Provence Rosé', 'Mourvèdre Rosé', 'Pinot Noir Rosé'];
+    if (reds.indexOf(varietal) > -1) {
+      return 'Red';
+    } else if (whites.indexOf(varietal) > -1) {
+      return 'White';
+    } else {
+      return 'Rose';
+    }
+  }
+
+  const fullSend = (type) => {
+    axios.post('http://localhost:3000/api/wine-list/create', {
+        winery: winery,
+        name: name,
+        wineType: type,
+        varietal: varietal,
+        vintage: vintage,
+        notes: notes,
+        purchaseAgain: purchaseAgain
+      })
+      .then(response => {
+        clearEntries();
+      })
+      .catch(error => {
+        console.log(`Error when creating entry: ${error}`);
+      });
+  }
+
+  const clearEntries = () => {
+    setWinery('');
+    setName('');
+    setVarietal('');
+    setVintage('');
+    setNotes('');
+  }
+
   return (
     <SafeAreaView>
-      <View>
-        <Text>WineForm!</Text>
-      </View>
+      <ScrollView>
+        <View>
+          <Text>Where's it from?</Text>
+          <TextInput
+            onChangeText={newWinery => setWinery(newWinery)}
+            placeholder='Winery'
+            maxLength={100}
+          />
+        </View>
+        <View>
+          <Text>What's it called?</Text>
+          <TextInput
+            onChangeText={newName => setName(newName)}
+            placeholder='Name'
+            maxLength={100}
+          />
+        </View>
+        <View>
+          <Text>Which varietal?</Text>
+          <TextInput
+            onChangeText={newVarietal => setVarietal(newVarietal)}
+            placeholder='Varietal'
+            maxLength={20}
+          />
+        </View>
+        <View>
+          <Text>Which year?</Text>
+          <TextInput
+            onChangeText={newVintage => setVintage(newVintage)}
+            placeholder='Vintage'
+            keyboardType={'numeric'}
+            maxLength={4}
+          />
+        </View>
+        <View>
+          <Text>Thoughts?</Text>
+          <TextInput
+            onChangeText={newNotes => setNotes(newNotes)}
+            placeholder='Notes'
+            maxLength={280}
+            multiline={true}
+          />
+        </View>
+        <View>
+          <Text>Buy Again?</Text>
+          <Picker
+            selectedValue={purchaseAgain}
+            onValueChange={currentRepeat => setRepeat(currentRepeat)}
+          >
+            <Picker.Item label='Yes' value='true' />
+            <Picker.Item label='No' value='false' />
+          </Picker>
+        </View>
+        <View>
+          <Button title='Save' onPress={submitWine()} />
+          <Button
+            title='Home'
+            onPress={() => {
+              navigation.navigate('Home');
+            }}
+        />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
